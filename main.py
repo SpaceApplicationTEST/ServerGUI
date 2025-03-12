a7f9a6bf4945c51769c079c12e9d9c70d9033c46
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-12 11:43:59 +0200

    Added function for adding space objects data

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello")
+import socket
+import pyodbc
+
+
+server = 'localhost'
+database = 'SpaceNotes'
+username = ''
+password = ''
+
+dsn = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};Trusted_Connection=yes'
+
+def commands(conn, request):
+    try:
+        if request.startswith('insert:'):
+            data = request[len('insert:'):]
+            name, diameter, howfar, type = data.split(',')
+
+            name = name.strip()
+            diameter = float(diameter.strip())
+            howfar = float(howfar.strip())
+            type_ = type.strip()
+
+            conn_db = pyodbc.connect(dsn)
+            cursor = conn_db.cursor()
+
+            insert_data = "INSERT INTO [Planets] ([name], [diameter], [howfar], [Type]) VALUES (?, ?, ?, ?)"
+            values = (name, diameter, howfar, type_)
+            cursor.execute(insert_data, values)
+            conn_db.commit()
+
+            result_str = "Data inserted"
+            conn.send(result_str.encode())
+            cursor.close()
+            conn_db.close()
